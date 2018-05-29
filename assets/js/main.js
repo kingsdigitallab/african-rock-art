56e873e0ad393f6ae10ce2c1604880bf90151172
Miguel Vieira <jmvieira@gmail.com>
2018-05-29 16:20:15 +0100

    Implemented a back to top for the gallery page.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,47 @@
 $(function() {
-  if ($("svg g#areas")) {
-    $("svg g#areas g").click(function() {
-      location.href = "country/" + this.id + "/";
-    });
+  if ($('svg g#areas')) {
+    $('svg g#areas g').click(function() {
+      location.href = 'country/' + this.id + '/'
+    })
 
-    $("svg g#areas g path").tooltipster({
+    $('svg g#areas g path').tooltipster({
       delay: 50,
       offset: [0, 30],
-      plugins: ["follower"],
-      theme: ["tooltipster-borderless"]
-    });
+      plugins: ['follower'],
+      theme: ['tooltipster-borderless']
+    })
   }
 
   // Expand/collapse
-  $(".accordion .chapter-title").click(function() {
+  $('.accordion .chapter-title').click(function() {
     $(this)
-      .toggleClass("opened closed")
-      .next(".chapter-content")
+      .toggleClass('opened closed')
+      .next('.chapter-content')
       .slideToggle(400)
-      .toggleClass("show hide")
-      .nextUntil(".chapter-title")
+      .toggleClass('show hide')
+      .nextUntil('.chapter-title')
       .slideToggle(400)
-      .toggleClass("show hide");
-    return false;
-  });
-});
+      .toggleClass('show hide')
+    return false
+  })
+
+  var amountScrolled = 300
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > amountScrolled) {
+      $('a.back-to-top').fadeIn('slow')
+    } else {
+      $('a.back-to-top').fadeOut('slow')
+    }
+  })
+
+  $('a.back-to-top').click(function() {
+    $('html, body').animate(
+      {
+        scrollTop: 0
+      },
+      700
+    )
+    return false
+  })
+})
