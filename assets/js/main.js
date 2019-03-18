c565820c7ed56c7aa4435c7b1cb6c07085c6660c
Miguel Vieira <jmvieira@gmail.com>
2019-03-18 20:23:06 +0000

    Reorganise the js to load the lazysizes scripts before the images start loading.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,48 @@
 $(function() {
-  if ($('svg g#areas')) {
-    $('svg g#areas g').click(function() {
-      location.href = 'country/' + this.id + '/'
-    })
+  if ($("svg g#areas")) {
+    $("svg g#areas g").click(function() {
+      location.href = "country/" + this.id + "/";
+    });
 
-    $('svg g#areas g path').tooltipster({
+    $("svg g#areas g path").tooltipster({
       delay: 50,
       offset: [0, 30],
-      plugins: ['follower'],
-      theme: ['tooltipster-borderless']
-    })
+      plugins: ["follower"],
+      theme: ["tooltipster-borderless"]
+    });
   }
 
   // Expand/collapse
-  $('.accordion .chapter-title').click(function() {
+  $(".accordion .chapter-title").click(function() {
     $(this)
-      .toggleClass('opened closed')
-      .next('.chapter-content')
+      .toggleClass("opened closed")
+      .next(".chapter-content")
       .slideToggle(400)
-      .toggleClass('show hide')
-      .nextUntil('.chapter-title')
+      .toggleClass("show hide")
+      .nextUntil(".chapter-title")
       .slideToggle(400)
-      .toggleClass('show hide')
-    return false
-  })
+      .toggleClass("show hide");
+    return false;
+  });
 
-  var offset = 300
+  var offset = 300;
 
   $(window).scroll(function() {
     if ($(window).scrollTop() > offset) {
-      $('a.top').fadeIn('slow')
+      $("a.top").fadeIn("slow");
     } else {
-      $('a.top').fadeOut('slow')
+      $("a.top").fadeOut("slow");
     }
-  })
+  });
 
-  $('a.top').click(function(event) {
-    event.preventDefault()
+  $("a.top").click(function(event) {
+    event.preventDefault();
 
-    $('html, body').animate(
+    $("html, body").animate(
       {
         scrollTop: 0
       },
       700
-    )
-  })
-})
+    );
+  });
+});
